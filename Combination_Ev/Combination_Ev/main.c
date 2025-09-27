c516188eda3224fd09ba4fd17fd95db3aeb94253
songforprism <eugod25@gmail.com>
2025-09-27 11:32:02 +0900

    Microchip studio project

---
+++ Combination_Ev/Combination_Ev/main.c
@@ -0,0 +1,18 @@
+/*
+ * Combination_Ev.c
+ *
+ * Created: 2025-09-27 오전 11:29:46
+ * Author : micha
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
